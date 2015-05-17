6961d76c8ae84091d95bddac0fae847b663ab5de
nutthaphon <nutthaphon@gmail.com>
2015-05-17 20:20:49 +0700

    Data Access Object creates Java model objects

---
+++ app/src/main/java/first/sqlite/android/vogella/de/testdatabase/Comment.java
@@ -0,0 +1,31 @@
+package first.sqlite.android.vogella.de.testdatabase;
+
+/**
+ * Created by esa on 17/5/2015.
+ */
+public class Comment {
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
---
+++ app/src/main/java/first/sqlite/android/vogella/de/testdatabase/MySQLiteHelper.java
@@ -0,0 +1,44 @@
+package first.sqlite.android.vogella.de.testdatabase;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by esa on 17/5/2015.
+ */
+public class MySQLiteHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE_COMMENTS = "comments";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_COMMENT = "comment";
+
+    public static final String DATABASE_NAME = "commments.db";
+    public static final int DATABASE_VERSION = 1;
+
+    // Database creation sql statement
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_COMMENTS + "(" + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_COMMENT
+            + " text not null);";
+
+
+    public MySQLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        Log.w(MySQLiteHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+        onCreate(sqLiteDatabase);
+    }
+}
